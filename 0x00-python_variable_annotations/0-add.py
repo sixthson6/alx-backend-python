5d639c6608239d620bb9ee9a15f52290667c2c8c
sixthson6 <sudais2408@gmail.com>
2024-10-10 14:30:07 +0000

    Add concat func

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Annotations in python"""
 
+
 def add(a: float, b: float) -> float:
     """add two floats"""
     return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Annotations in python"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Concat two strings"""
+    return str1 + str2
