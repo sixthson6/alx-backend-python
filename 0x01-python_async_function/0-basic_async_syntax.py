2df6789349deda16ff6f7bb178d8aafd8865bcdd
sixthson6 <sudais2408@gmail.com>
2024-10-15 23:19:55 +0000

    Add async func one

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -7,6 +7,10 @@ import asyncio
 
 
 async def wait_random(max_delay=10):
+    """
+    Generate a random float 
+    implement async funcs
+    """
     num = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return (num)
